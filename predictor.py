40ce352841cced4f5ab47f3ad6fc378471c08c83
Alvaro Leandro <leandro0807@live.com>
2021-07-25 14:07:10 -0300

    Adicionado função para plotar o previsor

--- predictor.py
+++ predictor.py
@@ -1,13 +1,12 @@
 import pandas as pd 
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
-from nltk.metrics.distance  import edit_distance
-import re
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 
+df = pd.read_csv('/home/alvaro/Área de Trabalho/data visualization/data/feature_frame.csv')
 
 y = df['y']
 df.drop(columns=['y'], inplace=True)
@@ -17,9 +16,9 @@ scaler = StandardScaler()
 
 X[['n_caracteristicas', 'metros', 'quartos', 'banheiros', 'vagas','descricao']] = scaler.fit_transform(X[['n_caracteristicas', 'metros', 'quartos', 'banheiros', 'vagas','descricao']])
 
-X = pd.get_dummies(X)
+# X = pd.get_dummies(X)
 
-# X['endereco'] = LabelEncoder().fit_transform(X['endereco'])
+X['endereco'] = LabelEncoder().fit_transform(X['endereco'])
 
 reg = LinearRegression().fit(X, y)
 reg.score(X, y)
@@ -38,16 +37,19 @@ clf.score(X, y)
 
 plt.barh(X.columns, clf.feature_importances_)
 
+def get_reg_plot(predictions, y):
+    plt.figure(figsize=(10,10))
+    plt.scatter(y, predictions, c='crimson')
+    plt.yscale('log')
+    plt.xscale('log')
 
-plt.figure(figsize=(10,10))
-plt.scatter(y, pred_reg, c='crimson')
-plt.yscale('log')
-plt.xscale('log')
+    p1 = max(max(predictions), max(y))
+    p2 = min(min(predictions), min(y))
+    plt.plot([p1, p2], [p1, p2], 'b-')
+    plt.xlabel('Valor real', fontsize=15)
+    plt.ylabel('Valor previsto', fontsize=15)
+    plt.axis('equal')
+    plt.show()
 
-p1 = max(max(pred_reg), max(y))
-p2 = min(min(pred_reg), min(y))
-plt.plot([p1, p2], [p1, p2], 'b-')
-plt.xlabel('True Values', fontsize=15)
-plt.ylabel('Predictions', fontsize=15)
-plt.axis('equal')
-plt.show()
+get_reg_plot(predictions, y)
+get_reg_plot(pred_reg, y)
